5bdf6a6c89db12918b1aadbd0b6a5ea1b4845060
Luis Vegerano <lvegerano422@gmail.com>
2015-02-10 22:57:12 -0600

    updated test but still they are screwing up :/

--- test/index.js
+++ test/index.js
@@ -34,7 +34,7 @@ describe('hapi-sql', function() {
             port: 3306,
             host: '192.168.59.103',
             logging: false,
-            syncOptions: {
+            sync: {
                 force: false
             }
         };
@@ -42,9 +42,7 @@ describe('hapi-sql', function() {
             register: require('..'),
             options: options
         }, function(err) {
-            //console.log(err);
             expect(err).to.be.undefined();
-            //expect(err).to.be.null;
             done();
         });
     });
@@ -52,14 +50,15 @@ describe('hapi-sql', function() {
     it('should error on bad database authentication', function(done) {
         var options = {
             modelsPath: '',
-            database: 'badTableName',
-            user: 'root',
+            logging: false,
+            database: 'test',
+            user: 'rot',
             password: 'vegerano',
             dialect: 'mysql',
             port: 3306,
             host: '192.168.59.103',
             logging: false,
-            syncOptions: {
+            sync: {
                 force: false
             }
         };
@@ -67,10 +66,72 @@ describe('hapi-sql', function() {
             register: require('..'),
             options: options
         }, function(err) {
-            //console.log(err);
-            expect(err).to.be.undefined();
-            //expect(err).to.be.null;
+            // TODO: need to try to be more specific about err
+            expect(err).to.be.an.object();
             done();
+        });
+    });
+
+    it('should error on bad database sync', function(done) {
+        // TODO: need to figure how to mock up a sync error
+        var options = {
+            modelsPath: '/test/modelsBad',
+            database: 'test',
+            user: 'root',
+            password: 'vegerano',
+            dialect: 'mysql',
+            port: 3306,
+            host: '192.168.59.103',
+            logging: false,
+            sync: {
+                force: true
+            }
+        };
+        server.register({
+            register: require('..'),
+            options: options
+        }, function(err) {
+            console.log(err);
+            expect(1).to.be.a.number();
+            done();
+        });
+    });
+
+    it('should expose models, sequelize, and Sequelize', function(done) {
+        var options = {
+            modelsPath: '/test/models',
+            database: 'test',
+            user: 'root',
+            password: 'vegerano',
+            dialect: 'mysql',
+            port: 3306,
+            host: '192.168.59.103',
+            logging: false,
+            sync: {
+                force: false
+            }
+        };
+        server.register({
+            register: require('..'),
+            options: options
+        }, function(err) {
+            console.log(server.plugins['hapi-sql']);
+            expect(server.plugins['hapi-sql'].database.sequelize).to.be.an.object();
+            expect(server.plugins['hapi-sql'].database.Sequelize).to.be.an.object();
+            expect(server.plugins['hapi-sql'].database.models).to.be.an.object();
+            done();
+            /*server.route({
+                method: 'GET',
+                path: '/',
+                handler: function(request, reply) {
+                    console.log(server.plugins);
+                    done();
+                }
+            });
+            server.inject({
+                method: 'GET',
+                path: '/'
+            }, function() {});*/
         });
     });
 });
