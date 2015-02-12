7ee6c999fab24bc7d89044ae19e664c1326d2db5
Luis Vegerano <lvegerano422@gmail.com>
2015-02-11 22:38:23 -0600

    removed sequlize.authenticat() as is really not necessary

--- test/index.js
+++ test/index.js
@@ -47,31 +47,6 @@ describe('hapi-sql', function() {
         });
     });
 
-    it('should error on bad database authentication', function(done) {
-        var options = {
-            modelsPath: '',
-            logging: false,
-            database: 'test',
-            user: 'rot',
-            password: 'vegerano',
-            dialect: 'mysql',
-            port: 3306,
-            host: '192.168.59.103',
-            logging: false,
-            sync: {
-                force: false
-            }
-        };
-        server.register({
-            register: require('..'),
-            options: options
-        }, function(err) {
-            // TODO: need to try to be more specific about err
-            expect(err).to.be.an.object();
-            done();
-        });
-    });
-
     it('should error on bad database sync', function(done) {
         // TODO: need to figure how to mock up a sync error
         var options = {
@@ -91,8 +66,7 @@ describe('hapi-sql', function() {
             register: require('..'),
             options: options
         }, function(err) {
-            console.log(err);
-            expect(1).to.be.a.number();
+            //expect(1).to.be.a.number();
             done();
         });
     });
@@ -115,23 +89,11 @@ describe('hapi-sql', function() {
             register: require('..'),
             options: options
         }, function(err) {
-            console.log(server.plugins['hapi-sql']);
+            //console.log(server.plugins['hapi-sql']);
             expect(server.plugins['hapi-sql'].database.sequelize).to.be.an.object();
             expect(server.plugins['hapi-sql'].database.Sequelize).to.be.an.object();
             expect(server.plugins['hapi-sql'].database.models).to.be.an.object();
             done();
-            /*server.route({
-                method: 'GET',
-                path: '/',
-                handler: function(request, reply) {
-                    console.log(server.plugins);
-                    done();
-                }
-            });
-            server.inject({
-                method: 'GET',
-                path: '/'
-            }, function() {});*/
         });
     });
 });
